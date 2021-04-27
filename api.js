c89db04e4c58f9e610cda9e358cd0f0a8380fe6b
Jannis R <mail@jannisr.de>
2021-04-27 15:06:06 +0200

    add mock fare

--- api.js
+++ api.js
@@ -6,6 +6,22 @@ const createCors = require('cors')
 const compression = require('compression')
 const pkg = require('./package.json')
 
+const mockFares = [{
+	// https://github.com/HSLdevcom/digitransit-ui/blob/8d756832f6b986a0d38a16db1e0f5774d4d19c64/app/util/fareUtils.js
+	type: 'regular',
+	currency: 'EUR',
+	cents: 370,
+	components: [{
+		fareId: 'foo',
+		currency: 'EUR',
+		cents: 370,
+		routes: [{
+			id: 'bar',
+			gtfsId: 'baz',
+		}],
+	}],
+}]
+
 const createApi = (cfg = {}) => {
 	const {
 		logger,
@@ -28,7 +44,7 @@ const createApi = (cfg = {}) => {
 	api.locals.logger = logger
 	api.set('etag', etags)
 
-	if (config.cors) {
+	if (cors) {
 		const cors = createCors({
 			origin: corsOrigin,
 			exposedHeaders: '*',
@@ -43,13 +59,20 @@ const createApi = (cfg = {}) => {
 		if (csp) res.setHeader('content-security-policy', csp)
 		res.setHeader('X-Powered-By', [pkg.name, pkg.version].join(' '))
 		res.setHeader('X-API-Version', pkg.version)
+		next()
 	})
 
 	api.use(compression())
+
 	// todo: prevent caching?
 	// todo: expose TRIAS response times via Server-Timing?
+	api.post('/fares', (req, res) => {
+		// todo: parse & validate req.body, send 400 on errors
 
-	// todo: ticket lookup route
+		// todo: find fares
+		// todo: generate ticket link
+		res.json(mockFares)
+	})
 
 	return api
 }
