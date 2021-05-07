b3a71f03dbc3a8f0f42714d3591ef0c6f9b0bebc
Jannis R <mail@jannisr.de>
2021-05-07 12:11:14 +0200

    basic E2E/integration test ✅

---
+++ test/index.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const execa = require('execa')
+const superagent = require('superagent')
+const assert = require('assert')
+const itGärtringenHerrenberg = require('./otp-itinerary-gärtringen-herrenberg.json')
+const gärtringenHerrenbergFares = require('./gärtringen-herrenberg-fares')
+
+const spawnApiServer = async () => {
+	console.debug('spawning API server as a child process')
+	const server = execa.node(require.resolve('./server'), {
+		env: {
+			LOG_LEVEL: process.env.LOG_LEVEL || 'warn',
+		},
+	})
+	server.stdout.pipe(process.stdout)
+	server.stderr.pipe(process.stderr)
+	server.once('close', (exitCode) => {
+		if (exitCode === null) return;
+		console.error(`server exited unexpectedly with exit code ${exitCode}`)
+		process.exit(exitCode || 1)
+	})
+	await new Promise(res => server.once('message', res))
+
+	const stop = () => {
+		console.debug('terminating API server child process')
+		server.kill('SIGTERM', {forceKillAfterTimeout: 1000})
+	}
+	return stop
+}
+
+;(async () => {
+	const stopApiServer = await spawnApiServer()
+
+	const res = await superagent
+	.post('http://localhost:3000/')
+	.set('content-type', 'application/json')
+	.set('accept', 'application/json')
+	.send(JSON.stringify(itGärtringenHerrenberg))
+
+	assert.strictEqual(res.statusCode, 200)
+	assert.strictEqual(res.headers['content-type'], 'application/json; charset=utf-8')
+	assert.deepStrictEqual(res.body, gärtringenHerrenbergFares)
+	console.info('✔︎ basic public transport works')
+
+	stopApiServer()
+})()
+.catch((err) => {
+	console.error(err)
+	process.exit(1)
+})
